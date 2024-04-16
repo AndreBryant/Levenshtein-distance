740f22e5ad365ae12fbfa3b10c0da54693a6b7a9
embryzon <95128949+embryzon@users.noreply.github.com>
2024-04-16 21:02:23 +0800

    faster implementatiopn of the levenshtein distance

--- Levenshtein.js
+++ Levenshtein.js
@@ -25,4 +25,51 @@ const Levenshtein = {
     }
 };
 
+// Faster
+export function levenshteinDistance(str1, str2) {
+    const m = str1.length;
+    const n = str2.length;
+
+    let dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i === 0) {
+                dp[i][j] = j;
+            } else if (j === 0) {
+                dp[i][j] = i;
+            } else {
+                dp[i][j] = Math.min(
+                    dp[i - 1][j - 1] + (str1[i - 1] !== str2[j - 1] ? 1 : 0),
+                    dp[i - 1][j] + 1,
+                    dp[i][j - 1] + 1
+                );
+            }
+        }
+    }
+    return dp[m][n];
+}
+
+
 export default Levenshtein;
+
+// //Levenshtein Distance (IDK yet if gagamitin ko pa mamaya)
+// export const levDistance = (str1: string, str2: string): number => {
+//     const m: number = str1.length;
+//     const n: number = str2.length;
+  
+//     let dp: number[][] = new Array(m+1).fill(0).map(()=>new Array(n+1).fill(0));
+    
+//     for (let i = 0; i <= m; i++) {
+//       for (let j = 0; j <= n; j++) {
+//         if (Math.min(i, j) === 0) {
+//           dp[i][j] = i + j;
+//         } else {
+//           dp[i][j] = Math.min( dp[i - 1][j - 1] + (str1[i - 1] !== str2[j - 1] ? 1 : 0),
+//           dp[i - 1][j] + 1,
+//           dp[i][j - 1] + 1)
+//         }
+//       }
+//     }
+//     return dp[m][n];
+//   }
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import { levenshteinDistance } from "./Levenshtein.js"
 import Levenshtein from "./Levenshtein.js"
 
-console.log(Levenshtein.distance('lex', 'hack'))
+console.log(levenshteinDistance('Male andre bryant', 'andre bryant male'))
+console.log(levenshteinDistance('cart', 'cra'))
+
+// This will run very slowly
+// console.log(Levenshtein.distance('Male andre bryan', 'andre bryant male'))
